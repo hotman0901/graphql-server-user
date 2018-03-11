5728f4a08aa30c0a0ab756f2bd76921abb275001
benny.hsieh <benny.hsieh@isuncloud.com>
2018-03-12 01:15:48 +0800

    增加company查詢功能，增加透過user內的companyId去關聯查詢compay的資料

--- schema/schema.js
+++ schema/schema.js
@@ -2,7 +2,7 @@
  * @Author: Benny 
  * @Date: 2018-03-10 20:03:55 
  * @Last Modified by: mikey.zhaopeng
- * @Last Modified time: 2018-03-10 22:04:08
+ * @Last Modified time: 2018-03-11 21:16:05
  * schema 必須跟api的欄位名稱一樣
  */
 const graphql = require('graphql');
@@ -10,7 +10,7 @@ const graphql = require('graphql');
 // 不使用假資料了
 // const _ = require('lodash');
 
-const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema, GraphQLList } = graphql;
 
 const axios = require('axios');
 // 先做假的
@@ -23,13 +23,23 @@ const axios = require('axios');
 // ]
 
 // 建立companyType 必須宣告在userType之前
+// 如果想要從company的查詢反推回去找user
+// 要使用GraphQLList這個方法
+// 又因為UserType的定義在這個方法之後，會導致取不到，所以fields要使用arrow function(整個文件完成之後才會被執行)
 const CompanyType = new GraphQLObjectType({
   name: 'Company',
-  fields: {
+  fields: () => ({
     id: { type: GraphQLString },
     name: { type: GraphQLString },
-    description: { type: GraphQLString }
-  }
+    description: { type: GraphQLString },
+    users: {
+      type: new GraphQLList(UserType),
+      resolve(parentValue, args) {
+        return axios.get(`http://localhost:3000/companies/${parentValue.id}/users`)
+        .then(res => res.data);
+      }
+    }
+  })
 });
 
 // 建立一個user的schema
@@ -42,11 +52,13 @@ const UserType = new GraphQLObjectType({
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
+    // 再去關聯company資料
     company: {
       type: CompanyType,
       resolve(parentValue, args) {
         // 再拿users內的companyId去companies內查詢資料
-        return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
+        return axios
+          .get(`http://localhost:3000/companies/${parentValue.companyId}`)
           .then(res => res.data);
       }
     }
@@ -60,6 +72,7 @@ const UserType = new GraphQLObjectType({
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
+    // 查詢uer資料
     user: {
       type: UserType,
       args: {
@@ -75,6 +88,18 @@ const RootQuery = new GraphQLObjectType({
         // 使用call api 做法json-server，因為是promise要用then
         return axios
           .get(`http://localhost:3000/users/${args.id}`)
+          .then(res => res.data);
+      }
+    },
+    // 查詢company資料
+    company: {
+      type: CompanyType,
+      args: {
+        id: { type: GraphQLString }
+      },
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:3000/companies/${args.id}`)
           .then(res => res.data);
       }
     }
